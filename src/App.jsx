b981179edb8066aa103ea8e968c402769ef775b6
benjamin zamora <benjazamo@gmail.com>
2024-01-19 01:21:47 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Project from './Components/Product';
+
+function App() {
+  return (
+    <div>
+      <Project name='Saludos'/>
+      <Project name='si'/>
+      <Project name='funciona'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Product.jsx
@@ -0,0 +1,9 @@
+const Project = ({name}) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  )
+}
+
+export default Project
