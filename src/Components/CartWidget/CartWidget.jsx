2c28b22ae733a0496b690fb0241561cf342cdb8e
benjamin zamora <benjazamo@gmail.com>
2024-01-26 04:06:58 -0300

    Netlify test

--- src/Components/CartWidget/CartWidget.jsx
+++ src/Components/CartWidget/CartWidget.jsx
@@ -3,8 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const CartWidget = ({amount}) => {
     return (
         <>
-            {/*<FontAwesomeIcon icon="fa-solid fa-cart-shopping" />*/}
-            <FontAwesomeIcon icon="fa-solid fa-basket-shopping" /> {amount}
+            <FontAwesomeIcon icon="fa-solid fa-cart-shopping" /> {amount}
         </>
     )
 }
