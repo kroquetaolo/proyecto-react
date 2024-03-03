9e599892e7ad7fa7fd222d6cb6a831041fa0d307
benjamin zamora <benjazamo@gmail.com>
2024-03-03 05:56:49 -0300

    ItemListContainer & itemList reorganized

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -2,27 +2,47 @@ import { useEffect, useState } from 'react';
 import './ItemListContainer.css';
 import { useParams } from 'react-router-dom';
 import ItemsList from '../Items/ItemsList';
+import { NavLink } from 'react-router-dom';
+import { BiCategory } from "react-icons/bi";
 import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore';
 
 const ItemListContainer = () => {
     const { categoryID } = useParams();
 
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const categories = ["Men's Clothing", "Women's Clothing", "Jewelry"];
 
     useEffect(() => {
         const db = getFirestore();
         const productRef = collection(db, 'products');
         const dataQuery = categoryID === undefined ? productRef : query(productRef, where("category", "==", categoryID));
+        setLoading(true)
         getDocs(dataQuery)
             .then(collection => {
                 const products = collection.docs.map(doc => doc.data());
+                setLoading(false)
                 setProducts(products);
             }).catch(err => console.error(err));
     }, [categoryID]);
 
     return (
         <div className='itemListContainer'>
-            <ItemsList productsList={products} />
+            {loading ? <div className='loader-container'><span className="loader"></span></div> :
+                <ItemsList productsList={products} />
+            }
+
+            <nav className='categories'>
+                <ul>
+                    <li><BiCategory /> Categories:</li>
+                    <li><NavLink to='/'>All</NavLink></li>
+                    {categories.map((category) => (
+                        <li key={category}>
+                            <NavLink to={`/category/${category}`}>{category}</NavLink>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
         </div>
     );
 };
--- src/Components/Items/ItemsList.jsx
+++ src/Components/Items/ItemsList.jsx
@@ -1,48 +1,14 @@
-import React, { useEffect, useState } from 'react';
 import Item from './Item';
-import { NavLink } from 'react-router-dom';
-import { BiCategory } from "react-icons/bi";
 import './Items.css'
 
 const ItemsList = ({ productsList }) => {
-    const categories = ["Men's Clothing", "Women's Clothing", "Jewelry"];
-    const [loading, setLoading] = useState(true);
-    const handleCategoryClick = () => {
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-        }, 600);
-    };
-
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false)
-        }, 600);
-    },[])
 
     return (
-        <React.Fragment>
-            { loading ? <div className='loader-container'><span className="loader"></span></div> :
-            <div className='content'>
-                {productsList.map((product) => (
-                    <React.Fragment key={product.id}>
-                        <Item itemProduct={product} />
-                    </React.Fragment>
-                ))}
-            </div>
-            }
-            <nav className='categories'>
-                <ul>
-                    <li><BiCategory /> Categories:</li>
-                    <li><NavLink onClick={handleCategoryClick} to={'/'}>All</NavLink></li>
-                    {categories.map((category) => (
-                        <li key={category}>
-                            <NavLink to={`/category/${category}`} onClick={handleCategoryClick}>{category}</NavLink>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </React.Fragment>
+        <div className='content'>
+            {productsList.map((product) => (
+                <Item key={product.id} itemProduct={product} />
+            ))}
+        </div>
     );
 };
 
